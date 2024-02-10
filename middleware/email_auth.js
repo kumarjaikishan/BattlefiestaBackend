a74b0c89c8830fc245d354559cda77653edef731
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-02-10 10:24:28 +0530

    vghgh

--- middleware/email_auth.js
+++ middleware/email_auth.js
@@ -23,7 +23,7 @@ const emailmiddleware = async (req, res, next) => {
             const mailOptions = {
                 from: 'Jai kishan',
                 to: query.email,
-                subject: 'Email Verification-Expense Management system',
+                subject: 'BattleFiesta Email Verification',
                 // html: `Hi ${query.name}, please <a href="https://esport-backend.vercel.app/verify?id=${query._id}" target="_blank">Click Here</a>  to Verify your Email,   Thanks for Joining Us, from Jai kishan(Developer)`
                 html: `<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
                 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
