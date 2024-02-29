3d00f63a45e8122188c9e97387cc1c1c2dad1a7d
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-02-29 21:53:07 +0530

    hgvhj

---
+++ modals/membership_schema.js
@@ -0,0 +1,41 @@
+const mongo = require('mongoose');
+
+const memberschema = new mongo.Schema({
+    userid: {
+        type: mongo.Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    plan_name: {
+        type: String,
+        required: true
+    },
+    buy_date: {
+        type: String,
+        required: true
+    },
+    expire_date: {
+        type: String,
+        required: true
+    },
+    coupon: {
+        type: String,
+        default:'',
+        required: false
+    },
+    city: {
+        type: String,
+        default:'',
+        required: false
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    finalpricepaid: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+const membership = new mongo.model("membership", memberschema);
+module.exports = membership;
