8506679489201df390b62f348a782e6825a5325f
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-03 22:03:42 +0530

    gvh

--- utils/trial_membership.js
+++ utils/trial_membership.js
@@ -3,14 +3,16 @@ const membership = require('../modals/membership_schema');
 
 const trialmembership = async (userid, planid) => {
     const today = new Date();
+    const threeMonthsLater = new Date(today);
+    threeMonthsLater.setMonth(today.getMonth() + 3);
     try {
         const query = new membership({
             userid, planid, txn_no: "Free Trial",
-            buy_date: today, expire_date: '2050-01-01', coupon: "",
+            buy_date: today, expire_date: threeMonthsLater, coupon: "",
             finalpricepaid: 0
         });
         const result = await query.save();
-        if(result){
+        if (result) {
             console.log('Free Trial created Successfully');
             return true;
         }
