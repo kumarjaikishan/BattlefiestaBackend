00da35d33a8afacec462124361a5e4e4de63d10c
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-31 16:11:06 +0530

    dfgg

--- controller/membership_controller.js
+++ controller/membership_controller.js
@@ -40,7 +40,7 @@ const manualcheck = asyncHandler(async (req, res, next) => {
         title: 'Membership Request',
         body: `Hey JaiKishan ${req.user.name} has applied for membership for Rs.${finalpricepaid}`,
     }
-    await push_notification(userid, mes)
+    await push_notification('65fc5697cec618fb1dcbd692', mes)
 
     return res.status(201).json({
         message: 'Request Submited'
