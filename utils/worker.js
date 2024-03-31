b2a251322b31a5e121ac44e1c1a135972ec948f2
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-03-31 15:56:54 +0530

    dfgr

--- utils/worker.js
+++ utils/worker.js
@@ -15,7 +15,7 @@ async function sendEmail(job) {
 
 const worker = new Worker('battlefiesta_queue', sendEmail, {
     connection: new IORedis(process.env.REDIS_URIfulle, {
-        maxRetriesPerRequest: null,
+        maxRetriesPerRequest: 2,
     }),
 });
 
