56f934d64c97db36c5d6600614241e778f87c992
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-10-27 13:06:39 +0530

    frfr

--- utils/trial_membership.js
+++ utils/trial_membership.js
@@ -2,15 +2,17 @@ const membership = require('../modals/membership_schema');
 
 const trialmembership = async (userid, planid) => {
     const today = new Date();
-    const currentTime = new Date(today.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
+    const istOffset = 5 * 60 + 30; // IST is UTC+5:30
+    const todayIST = new Date(today.getTime() + istOffset * 60 * 1000);
 
-    const threeMonthsLater = new Date(currentTime);
-    threeMonthsLater.setMonth(currentTime.getMonth() + 3);
+    // Calculate three months later in IST
+    const threeMonthsLater = new Date(todayIST);
+    threeMonthsLater.setMonth(todayIST.getMonth() + 3);
 
     try {
         const query = new membership({
             userid, planid, txn_no: "Free Trial",
-            buy_date: currentTime,
+            buy_date: todayIST,
             expire_date: threeMonthsLater,
             coupon: "",
             finalpricepaid: 0
