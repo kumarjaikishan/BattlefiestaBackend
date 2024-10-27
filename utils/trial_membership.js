9600b8ff17663dc6ddf7675bd2e4befd701c9e40
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-10-27 13:15:45 +0530

    cfd

--- utils/trial_membership.js
+++ utils/trial_membership.js
@@ -2,29 +2,36 @@ const membership = require('../modals/membership_schema');
 
 const trialmembership = async (userid, planid) => {
     const today = new Date();
-    const istOffset = 5 * 60 + 30; // IST is UTC+5:30
-    const todayIST = new Date(today.getTime() + istOffset * 60 * 1000);
+    const threeMonthsLater = new Date(today);
+    
+    // Convert to IST
+    const ISTOffset = 330; // IST is UTC+5:30
+    const offset = ISTOffset * 60 * 1000; // Convert offset to milliseconds
+    
+    const todayIST = new Date(today.getTime() + offset);
+    const threeMonthsLaterIST = new Date(threeMonthsLater.getTime() + offset);
+    
+    threeMonthsLaterIST.setMonth(threeMonthsLaterIST.getMonth() + 3);
 
-    // Calculate three months later in IST
-    const threeMonthsLater = new Date(todayIST);
-    threeMonthsLater.setMonth(todayIST.getMonth() + 3);
-
+    console.log("today IST",todayIST)
+    console.log("3months later IST",threeMonthsLaterIST)
+    
     try {
         const query = new membership({
             userid, planid, txn_no: "Free Trial",
             buy_date: todayIST,
-            expire_date: threeMonthsLater,
+            expire_date: threeMonthsLaterIST,
             coupon: "",
             finalpricepaid: 0
         });
         const result = await query.save();
         if (result) {
-            // console.log('Free Trial created Successfully');
+            console.log('Free Trial created Successfully');
             return true;
         }
     } catch (error) {
         console.log('create trial membership', error);
-        return false;
     }
-}
+};
+
 module.exports = trialmembership;
