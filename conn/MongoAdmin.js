0195980cd64f47abaea157870ff1d842265b5efc
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2025-12-28 17:03:47 +0530

    vfd

--- conn/MongoAdmin.js
+++ conn/MongoAdmin.js
@@ -2,13 +2,23 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.basemongo;
 
+if (!uri) {
+  throw new Error("MongoDB URI (basemongo) not found in environment variables");
+}
+
 let client;
 let clientPromise;
 
-export const getMongoClient = async () => {
+const getMongoClient = async () => {
   if (!clientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, {
+      maxPoolSize: 10 // optional but recommended
+    });
+
     clientPromise = client.connect();
   }
+
   return clientPromise;
 };
+
+module.exports = { getMongoClient };
